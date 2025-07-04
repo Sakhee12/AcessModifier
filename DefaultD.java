66de8e1a22bfd3855fd2da712183cf5c8b34c896
Sakhee12 <157628964+Sakhee12@users.noreply.github.com>
2025-07-04 22:54:15 +0530

    Add files via upload

---
+++ DefaultD.java
@@ -0,0 +1,12 @@
+package accessModifierPack1;
+
+import accessModifier.DefaultC;
+
+public class DefaultD 
+{
+	public static void main(String[] args) 
+	{
+		DefaultC c1 = new DefaultC();
+		DefaultC.test();
+	}
+}
